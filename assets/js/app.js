d9560ba04e563fdcdd8226c94fe8e637fb9a2478
Huseyn-Ismayilov <info.cyweb@gmail.com>
2023-12-09 13:13:35 +0400

    #

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,37 +52,6 @@ $(document).click(function (event) {
 // });
 
 
-
-function makeTimer() {
-
-	var endTime = new Date("6 December 2023 9:56:00 GMT+04:00");
-	endTime = (Date.parse(endTime) / 1000);
-
-	var now = new Date();
-	now = (Date.parse(now) / 1000);
-
-	var timeLeft = endTime - now;
-
-	var days = Math.floor(timeLeft / 86400);
-	var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-	var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-	var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-	if (days < "10") { days = "0" + days; }
-	if (hours < "10") { hours = "0" + hours; }
-	if (minutes < "10") { minutes = "0" + minutes; }
-	if (seconds < "10") { seconds = "0" + seconds; }
-
-	$("#days > div").html(days + "<span>Days</span>");
-	$("#hours > div").html(hours + "<span>Hours</span>");
-	$("#minutes > div").html(minutes + "<span>Minutes</span>");
-	$("#seconds > div").html(seconds + "<span>Seconds</span>");
-
-}
-
-setInterval(function () { makeTimer(); }, 1000);
-
-
 // Show the first tab and hide the rest
 $('#tabs-nav li:first-child').addClass('active');
 $('.tab-content').hide();
